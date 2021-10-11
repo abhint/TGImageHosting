c6b4245c93d6df3dd19ef0ff3f30050c3f802d17
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 12:00:59 +0530

    img uploader

---
+++ bot/workers/img_uploader.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def img_uploader(file_path: str):
+    _params = {
+        'key': '9076a9d6de80de9fc13ea32268c664ea',
+    }
+
+    _files = {'image': open(file_path, 'rb')}
+
+    response = requests.post('https://api.imgbb.com/1/upload',
+                             params=_params,
+                             files=_files).json()
+    return response
