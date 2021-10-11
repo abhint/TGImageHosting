72183229e663569ad0b45dbf41a583c548dd82db
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 14:29:09 +0530

    requests error

--- bot/workers/img_uploader.py
+++ bot/workers/img_uploader.py
@@ -2,13 +2,16 @@ import requests
 
 
 async def img_uploader(file_path: str):
-    _params = {
-        'key': '9076a9d6de80de9fc13ea32268c664ea',
-    }
+    try:
+        _params = {
+            'key': '9076a9d6de80de9fc13ea32268c664ea',
+        }
 
-    _files = {'image': open(file_path, 'rb')}
+        _files = {'image': open(file_path, 'rb')}
 
-    response = requests.post('https://api.imgbb.com/1/upload',
-                             params=_params,
-                             files=_files).json()
-    return response
+        response = requests.post('https://api.imgbb.com/1/upload',
+                                 params=_params,
+                                 files=_files).json()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(e)
