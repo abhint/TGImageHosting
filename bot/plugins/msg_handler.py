1cdfa32349b436caf49af14aaa71db49f98a67bb
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 12:01:26 +0530

    call img_uploader def

--- bot/plugins/msg_handler.py
+++ bot/plugins/msg_handler.py
@@ -1,7 +1,11 @@
 from ..IMGClient import IMG
 from pyrogram import filters
+from pyrogram.types import Message
+from ..workers.img_uploader import img_uploader
 
 
 @IMG.on_message(filters.photo)
-async def onPhotho(_, msg):
-    await msg.download(file_name="hi.jpg")
+async def onPhotho(client, msg):
+    file_path = await msg.download(file_name=f"downloads/{msg.chat.id}/")
+    see = img_uploader(file_path=file_path)
+    print(see['data']['url'])
