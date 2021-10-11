66a2b41d65c779013bd721cc1cce46753134ec8c
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 15:10:36 +0530

    blod text and error handling

--- bot/plugins/msg_handler.py
+++ bot/plugins/msg_handler.py
@@ -7,8 +7,16 @@ from ..workers.img_uploader import img_uploader
 @IMG.on_message(filters.photo)
 async def onPhotho(client, msg):
     file_path = await msg.download(file_name=f"downloads/{msg.chat.id}/")
-    responce = await img_uploader(file_path=file_path)
+    responce = await img_uploader(file_path=file_path, msg=msg)
+    if (responce is None):
+        return
+    title = responce['data']['title']
+
     # print(see['data']['url'])
-    client.send_message(chat_id=msg.chat.id,
-                        text="hello",
-                        reply_to_message_id=msg.message_id)
+    await client.send_message(
+        chat_id=msg.chat.id,
+        text=
+        f"**Name: {title}\nDirect URL: [{title}]({responce['data']['url']})**",
+        parse_mode="markdown",
+        reply_to_message_id=msg.message_id,
+    )
