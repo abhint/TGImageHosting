25859d97079e64917564575e8bea58262914c655
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 12:07:57 +0530

    send responce

--- bot/plugins/msg_handler.py
+++ bot/plugins/msg_handler.py
@@ -7,5 +7,8 @@ from ..workers.img_uploader import img_uploader
 @IMG.on_message(filters.photo)
 async def onPhotho(client, msg):
     file_path = await msg.download(file_name=f"downloads/{msg.chat.id}/")
-    see = await img_uploader(file_path=file_path)
-    print(see['data']['url'])
+    responce = await img_uploader(file_path=file_path)
+    # print(see['data']['url'])
+    client.send_message(chat_id=msg.chat.id,
+                        text="hello",
+                        reply_to_message_id=msg.message_id)
