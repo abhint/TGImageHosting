1c364a43bd65222875cd46e15ef659fa7f5247fe
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 15:09:14 +0530

    json error

--- bot/workers/img_uploader.py
+++ bot/workers/img_uploader.py
@@ -1,7 +1,7 @@
 import requests
 
 
-async def img_uploader(file_path: str):
+async def img_uploader(file_path: str, msg=None):
     try:
         _params = {
             'key': '9076a9d6de80de9fc13ea32268c664ea',
@@ -9,9 +9,11 @@ async def img_uploader(file_path: str):
 
         _files = {'image': open(file_path, 'rb')}
 
-        response = requests.post('https://api.imgbb.com/1/upload',
-                                 params=_params,
-                                 files=_files).json()
+        _response = requests.post('https://api.imgbb.com/1/upload',
+                                  params=_params,
+                                  files=_files)
+        response = _response.json()
         return response
-    except requests.exceptions.RequestException as e:
-        print(e)
+    except Exception as e:
+        await msg.reply('Uploading error 😓')
+        return
