4e59bc34c7d68216642b67b17e506620edc57227
AbhijithNT <abhijithnt64@gmail.com>
2021-10-11 20:44:44 +0530

    remove print()

--- bot/plugins/msg_handler.py
+++ bot/plugins/msg_handler.py
@@ -9,7 +9,6 @@ async def onPhotho(client, msg):
     client_msg = await client.send_message(chat_id=msg.chat.id,
                                            text="Downloading...",
                                            reply_to_message_id=msg.message_id)
-    print(client_msg)
     file_path = await msg.download(file_name=f"downloads/{msg.chat.id}/")
     responce = await img_uploader(file_path=file_path, msg=client_msg)
     if (responce is None):
